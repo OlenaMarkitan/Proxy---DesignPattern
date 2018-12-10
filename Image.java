81a50564e032cad73760a28e26b9ef0cb07c5e89
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-10 21:19:07 +0200

    Add files via upload

---
+++ Image.java
@@ -0,0 +1,7 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Structural_Patterns.Proxy.Proxy_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/proxy_pattern.htm
+
+public interface Image {
+    void display();
+}
